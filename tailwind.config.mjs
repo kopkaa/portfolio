002c1b8f2b2ba076c59978805e4a62bc9c593acb
Jan Hromádka <cumlikator1@email.cz>
2024-07-23 17:23:51 +0200

    Added hero section, image, custom colors
    added fonts
    
    Updated background
    
    Styles update
    
    Improvements
    
    Blue color redefinition
    
    Updated portfolio according to template
    
    Little tweaks

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        green: {
+          200: '#ccf381',
+        },
+        blue: {
+          200: '#4831d4',
+					800: '#06083d'
+        },
+				purple: {
+					300: '#af43be',
+					400: '#fe00fe'
+				}
+      },
+      fontFamily: {
+        nunito: ['nunito', 'sans-serif'],
+        rethink: ['Rethink Sans', 'sans-serif'],
+        lexend: ['Lexend Mega', 'sans-serif'],
+      },
+			dropShadow: {
+        glow: [
+          "0 0px 20px rgba(255,255, 255, 0.35)",
+          "0 0px 65px rgba(255, 255,255, 0.2)"
+        ]
+      }
+    },
+  },
+  plugins: [],
+};
